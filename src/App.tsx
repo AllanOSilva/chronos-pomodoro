84a5e8db0d6ac2a43a9533d79a75abae6cd20ffc
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2026-01-12 22:43:59 -0300

    heading inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react';
 
 export function App(){
-    console.log("oi");
+    
     return (
         <>
-            <Heading />
+            <Heading>Olá mundo!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi eum voluptatibus esse illum provident laboriosam, suscipit deleniti voluptas qui! Magnam beatae labore perspiciatis nulla? Natus, ex error. Saepe, placeat deleniti?</p>
         </>
     );
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,12 @@
 import styles from './Heading.module.css';
 
-export function Heading(){
-    console.log(styles);
-    
+type HeadingProps ={
+    children:React.ReactNode;
+}
+
+export function Heading({ children }: HeadingProps){
+   
     return(
-    <h1 className={styles.heading}>Olá Mundo!</h1>
+    <h1 className={styles.heading}>{children}</h1>
     );
 }
