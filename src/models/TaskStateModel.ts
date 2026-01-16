2efaf3fcd6d231b1c035a9bdce236e868695783e
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2026-01-16 18:09:54 -0300

    c

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,15 +1,15 @@
 import type { TaskModel } from "./TaskModel"
 
-
+//estado -> componente -> filhos
 export type TaskStateModel={
-    tasks: TaskModel[];
-    secondsRemaining: number;
-    formattedSecondsRemaining: string;
-    activeTask:TaskModel|null;
-    currentCycle:number;//1 ao 8
+    tasks: TaskModel[];//histórico, mainform, ...
+    secondsRemaining: number;//countdown, histórico, mainform, botão
+    formattedSecondsRemaining: string; // título, countDown
+    activeTask:TaskModel|null;// título, countDown, mainform, button
+    currentCycle:number;//1 ao 8 //home
     config:{
-        workTime:number;
-        shortBreakTime:number;
-        longBreakTime:number;
+        workTime:number;//mainform
+        shortBreakTime:number;//mainform
+        longBreakTime:number;//mainform
     };
 };
